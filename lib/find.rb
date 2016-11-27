d4fcbd993af4aa1b411a2242da694172906ec94d
Andrew <Andreww716@gmail.com>
2016-11-26 22:07:04 -0800

    Add specs for words that are capitalized, accounts for common punctuation.

--- lib/find.rb
+++ lib/find.rb
@@ -2,9 +2,9 @@ class String
   define_method(:find) do |word|
     match_array = []
     counter = 0
-    sentence = self.split(" ")
+    sentence = self.downcase.split(" ")
     sentence.each() do |item|
-      if item === word
+      if (item === word) || (item === word + ".") || (item === word + "!") || (item === word + "?") || item === '"' + word + '"'
         counter += 1
       end
     end
--- spec/find_spec.rb
+++ spec/find_spec.rb
@@ -2,7 +2,16 @@ require("rspec")
 require("find")
 
 describe("String#find") do
+  it("Will compare two string inputs and let the user know if the word they entered matches the word they specified by showing them a counter.") do
+    expect("cat".find("cat")).to(eq(1))
+  end
   it("Will compare the input to the argument to see if they are equal, and if they are, update the counter to reflect how many times they're equal.") do
     expect("cat cat cat".find("cat")).to(eq(3))
+  end
+  it("Will count the word even if it ends a sentence.") do
+    expect("cat cat cat.".find("cat")).to(eq(3))
+  end
+  it("Will count the word even if it is inside quotes") do
+    expect('cat! "cat" cat. cat?'.find("cat")).to(eq(4))
   end
 end
